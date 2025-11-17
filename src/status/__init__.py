22f9a72e954e477ca76bbeb772bfdbe04d3b8ee2
Diego <134800819+D1TCRY@users.noreply.github.com>
2025-11-17 01:10:56 +0100

    Delete src/status directory

--- src/status/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Sprite import Sprite
-from .State import State
-from .states import Action, Direction, Phase
-from .SpriteCollection import SpriteCollection
-
-__all__ = ["Sprite", "State", "Action", "Direction", "Phase", "SpriteCollection"]
