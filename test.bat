8dce391e7c090b9ef1ae4635b51ba28807587e6f
Diego <134800819+D1TCRY@users.noreply.github.com>
2025-11-21 17:46:11 +0100

    Delete test.bat

--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "current_path=%~dp0"
-cd /d "%current_path%"
-call env\Scripts\activate
-pip install -r req.txt
-python -m unittest discover -s tests -p "test_*.py"
-call deactivate
-PAUSE
